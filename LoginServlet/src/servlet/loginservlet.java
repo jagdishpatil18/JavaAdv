f65eef7bfb6d8fa38973de1b493c02b2c5d536a8
Jagdish Patil <jagdishpatil477@gmail.com>
2017-04-26 19:49:22 +0530

    servlet

--- LoginServlet/src/servlet/loginservlet.java
+++ LoginServlet/src/servlet/loginservlet.java
@@ -1,42 +1,45 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class loginservlet
- */
-@WebServlet("/loginservlet")
-public class loginservlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public loginservlet() 
-    {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+public class loginservlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+     
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		PrintWriter printWriter=response.getWriter();
+//		printWriter.println("<h1>"+message+"</h1>");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		PrintWriter printWriter=response.getWriter();
+//		printWriter.println("<h1>"+message+"</h1>");
+		printWriter.println("Hello");
+		
+		String emailid=request.getParameter("email");
+		String passwd=request.getParameter("password");
+		printWriter.write(emailid+"   "+passwd);
+		
+//		ServletConfig config=getServletConfig();
+//		String driver=config.getInitParameter("driver");
+//		printWriter.println(driver);
+	
+	
 	}
-
+    public void destroy()
+    {
+    	
+    }
 }
